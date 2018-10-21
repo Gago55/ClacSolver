9c53d2986127431218566c0b40042976c83225c6
gago55 <xgagik8@gmail.com>
2018-10-22 02:21:06 +0400

    added `     .class Element     .constructor with 2 arg     .constructor with one arg

---
+++ CalcSolver/CalcSolver/Element.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcSolver
+{
+    class Element
+    {
+        public Instruments inst = new Instruments();
+        public int x;
+
+        public Element(Instruments instrument,int x)
+        {
+            inst = instrument;
+            this.x = x;
+        }
+
+        public Element(Instruments instrument)
+        {
+            inst = instrument;
+        }
+    }
+}
