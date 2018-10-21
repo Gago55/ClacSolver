d9af4c5e97d26d2249b155482dacbc3ce729855d
gago55 <xgagik8@gmail.com>
2018-10-22 02:17:01 +0400

    added `     .enum Instruments

---
+++ CalcSolver/CalcSolver/Instruments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcSolver
+{
+    enum Instruments
+    {
+        P2M,
+        PLUS,
+        MINUS,
+        MULT,
+        DIV,
+        REV,
+        REMOVE
+    }
+}
