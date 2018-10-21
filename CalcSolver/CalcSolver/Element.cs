4f6214e79c064a5af31b8c1d0bcc90aec4e076a4
gago55 <xgagik8@gmail.com>
2018-10-22 02:41:25 +0400

    added `     .property oneArgument     .method Do

--- CalcSolver/CalcSolver/Element.cs
+++ CalcSolver/CalcSolver/Element.cs
@@ -8,16 +8,56 @@ namespace CalcSolver
     {
         public Instruments inst = new Instruments();
         public int x;
+        bool oneArgument;
 
         public Element(Instruments instrument,int x)
         {
             inst = instrument;
             this.x = x;
+            oneArgument = false;
         }
 
         public Element(Instruments instrument)
         {
             inst = instrument;
+            oneArgument = true;
+        }
+
+        public void Do(ref int value)
+        {
+            if(oneArgument)
+            {
+                switch (inst)
+                {
+                    case Instruments.P2M:
+                        P2M(ref value);
+                        break;
+                    case Instruments.REMOVE:
+                        RemoveLast(ref value);
+                        break;
+                    case Instruments.REV:
+                        Reverse(ref value);
+                        break;
+                }
+            }
+            else
+            {
+                switch (inst)
+                {
+                    case Instruments.PLUS:
+                        Plus(ref value , x);
+                        break;
+                    case Instruments.MINUS:
+                        Minus(ref value, x);
+                        break;
+                    case Instruments.MULT:
+                        Multiplication(ref value, x);
+                        break;
+                    case Instruments.DIV:
+                        Division(ref value, x);
+                        break;
+                }
+            }
         }
     }
 }
