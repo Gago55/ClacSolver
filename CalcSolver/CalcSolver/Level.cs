b8c662c680b6a8df84eddb00b67b69a3b7dbbba5
gago55 <xgagik8@gmail.com>
2018-10-22 02:27:20 +0400

    added `     .class Level     .properties     .constructor

---
+++ CalcSolver/CalcSolver/Level.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcSolver
+{
+    class Level
+    {
+        public int moves ;
+        public int goal ;
+        public int value ;
+        public int instruments ;
+        public List<Element> elements;
+
+        public Level(int moves ,int value, int goal , int instruments , List<Element> elements)
+        {
+            this.moves = moves;
+            this.goal = goal;
+            this.value = value;
+            this.instruments = instruments;
+            this.elements = elements;
+        }
+
+
+    }
+}
