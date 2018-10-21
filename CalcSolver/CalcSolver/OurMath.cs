2b674ff5104b8531ef6030e8b3ef9bef29a026b4
gago55 <xgagik8@gmail.com>
2018-10-22 01:23:04 +0400

    added `     .method Plus     .method Minus

--- CalcSolver/CalcSolver/OurMath.cs
+++ CalcSolver/CalcSolver/OurMath.cs
@@ -6,5 +6,14 @@ namespace CalcSolver
 {
     class OurMath
     {
+        public void Plus(ref int value , int x)
+        {
+            value += x;
+        }
+
+        public void Minus(ref int value, int x)
+        {
+            value -= x;
+        }
     }
 }
