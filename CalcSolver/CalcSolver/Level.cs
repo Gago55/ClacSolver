c709260e68c1ebe3c208e1f359d18e675a7af724
gago55 <xgagik8@gmail.com>
2018-10-23 18:37:35 +0400

    added `     .methods RotateRight , Permutate (from INet)     .filled method Solve , now it solve only then moves equal to instruments count

--- CalcSolver/CalcSolver/Level.cs
+++ CalcSolver/CalcSolver/Level.cs
@@ -6,13 +6,13 @@ namespace CalcSolver
 {
     class Level
     {
-        public int moves ;
-        public int goal ;
-        public int value ;
-        public int instruments ;
+        public int moves;
+        public int goal;
+        public int value;
+        public int instruments;
         public List<Element> elements;
 
-        public Level(int moves ,int value, int goal , int instruments , List<Element> elements)
+        public Level(int moves, int value, int goal, int instruments, List<Element> elements)
         {
             this.moves = moves;
             this.goal = goal;
@@ -21,6 +21,47 @@ namespace CalcSolver
             this.elements = elements;
         }
 
+        public void Solve()
+        {
+            foreach (var permu in Permutate(elements, elements.Count))
+            {
+                int startValue = value;
+                foreach (var i in permu)
+                {
+                    i.Do(ref value);
+                }
+                if (value != goal)
+                    value = startValue;
+                else
+                {
+                    foreach (var i in permu)
+                    {
+                        Console.Write(i.inst.ToString() + " ");
+                    }
+                    break;
+                }
+            }
+        }
 
+        public static void RotateRight(List<Element> sequence, int count)
+        {
+            Element tmp = sequence[count - 1];
+            sequence.RemoveAt(count - 1);
+            sequence.Insert(0, tmp);
+        }
+
+        public static IEnumerable<List<Element>> Permutate(List<Element> sequence, int count)
+        {
+            if (count == 1) yield return sequence;
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    foreach (var perm in Permutate(sequence, count - 1))
+                        yield return perm;
+                    RotateRight(sequence, count);
+                }
+            }
+        }
     }
 }
